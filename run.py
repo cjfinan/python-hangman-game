e5194c50cb9286cc29a6a786ebbc648f994e9f89
cjfinan <cjfinan98@gmail.com>
2022-09-20 08:25:28 +0000

    [ADD] intial commit

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+keywords = ["computer", "python", "code", "institute"]
+
+
+def get_user_guess():
+    """
+    Asks user to guess a letter. Run a while loop to collect a valid input,
+    which must be a letter between a-z
+    """
+    while True:
+        print("Please enter the letter you would like to guess!")
+        print("It must be between a-z and lowercase\n")
+
+        user_guess = input(str("Enter your letter here:\n"))
+
+        if validate_guess(user_guess):
+            print("Guess is valid!")
+            break
+    
+    return user_guess
+
+
+
+get_user_guess()
